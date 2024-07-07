b21be79f18eb9cd60b4ed0859db4056e1125efbb
lefortnuno <trofelnuno@gmail.com>
2024-07-08 00:58:27 +0100

    Suppression des fichiers absents localement

--- Programmation Concurentiel/TP/MessageCryptE/ConfigServeur.java
+++
@@ -1,6 +0,0 @@
-public class ConfigServeur {
-    public static final String SERVER_ADDRESS = "192.168.1.17";
-    public static final String SECRET_KEYX = "L4AnoP$.@#75Nolo";
-    public static final int PORT_NUMBER = 3002;
-    public static final int BIT_NUMBER = 128;
-}
